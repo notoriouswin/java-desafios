8d0d7d0c7930a42d3495fe97c33e37f6bb6c37df
notoriouswin <bessadev13@gmail.com>
2026-06-15 07:28:26 -0300

    update

---
+++ 02-repeticao-busca/BuscaBinariaExe.java
@@ -0,0 +1,14 @@
+public class BuscaBinariaExe {
+    public static void main(String[]args){
+        int numeroSecreto = 84;
+        int minimo = 1;
+        int maximo = 100;
+        int tentativas = 0;
+        boolean encontrou = false;
+        if (numeroSecreto > 50){
+
+        } else if (numeroSecreto < 50){
+            encontrou = true;
+        }
+    }
+}
